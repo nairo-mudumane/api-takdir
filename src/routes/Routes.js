bfde8ea717af7a9cb21bbcee02e35380e259f359
NairoMudumane <nairomudumane@outlook.com>
2021-09-13 14:25:07 +0200

    adding home route

---
+++ src/routes/Routes.js
@@ -0,0 +1,35 @@
+// Route list
+exports.get = (req, res) => {
+  res.send([
+    {
+      name: `user list`,
+      method: `GET`,
+      url: `hostname/users/`,
+    },
+    {
+      name: `user by ID`,
+      method: `GET`,
+      url: `hostname/user/:user_id`,
+    },
+    {
+      name: `post new user`,
+      method: `POST`,
+      url: `hostname/user/new`,
+    },
+    {
+      name: `product list`,
+      method: `GET`,
+      url: `hostname/products/`,
+    },
+    {
+      name: `product by ID`,
+      method: `GET`,
+      url: `hostname/product/:product_id`,
+    },
+    {
+      name: `post new product`,
+      method: `POST`,
+      url: `hostname/product/new`,
+    },
+  ]);
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,12 @@
 const getUserList = require('./getUserList');
 const getProductList = require('./getProductList');
+const Route = require('./Routes');
 module.exports = (App) => {
+  App.get('/', (req, res) => res.send('No body returned for response'));
+
   getUserList(App);
   getProductList(App);
+  App.get('/routes', Route.get);
+
+  App.get('/*', (req, res) => res.redirect('/'));
 };
