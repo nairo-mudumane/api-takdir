bfe7a0ea62fea09f9b8b437501096f49981284ac
nairo-mudumane <nazirmudumane@gmail.com>
2022-01-13 17:33:57 +0200

    updated products route

--- src/controllers/Products.js
+++ src/controllers/Products.js
@@ -21,3 +21,64 @@ exports.getAll = async (req, res, next) => {
     return res.status(400).send(data);
   }
 };
+
+exports.getById = async (req, res, next) => {
+  const { id_product } = req.params;
+  const list = [];
+
+  try {
+    await db.ref(product_list_ref).once("value", (snapshot) => {
+      snapshot.forEach((product) => {
+        list.push(product.val());
+      });
+
+      if (list.length === 0) {
+        const data = { error: true, message: "not found" };
+        return res.status(404).send(data);
+      } else {
+        list.forEach((product) => {
+          if (product.uid === id_product) {
+            const data = { error: false, status: "ok" };
+            return (data["data"] = product);
+          }
+        });
+        return res.status(200).send(data);
+      }
+    });
+  } catch (err) {
+    const data = { error: true, message: err.message };
+    return res.status(400).send(data);
+  }
+};
+
+// exports.post = (req, res, next) => {
+//   const data = req.body;
+//   if (!data)
+//     return res.status(204).send([{ status: "ERROR", msg: "No Content" }]);
+
+//   const postProduct = async () => {
+//     const { v4: uuidv4 } = require("uuid");
+//     data["uid"] = uuidv4();
+//     await db
+//       .ref(product_list_ref)
+//       .child(data.uid)
+//       .set(data)
+//       .catch((err) => console.log(err));
+//   };
+//   postProduct();
+//   return res.status(200).send([{ status: "OK", msg: "Created" }]);
+// };
+
+// exports.put = (req, res, next) => {
+//   const product_uid = req.params.product_uid;
+//   res
+//     .status(201)
+//     .send([{ status: "OK", msg: `rota update product by id: ${product_uid}` }]);
+// };
+
+// exports.delete = (req, res, next) => {
+//   const product_uid = req.params.user_uid;
+//   res
+//     .status(201)
+//     .send([{ status: "OK", msg: `rota delete by id: ${user_uid}` }]);
+// };
--- src/routes/Products.js
+++ src/routes/Products.js
@@ -2,4 +2,5 @@ const Controller = require("../controllers/Products");
 
 module.exports = (App) => {
   App.get("/products", Controller.getAll);
+  App.get("/products/:id_product", Controller.getById);
 };
